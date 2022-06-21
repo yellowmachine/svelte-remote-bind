3551bb25c84e62d1bcf34ac0fd47d1399aec36d6
Yellow Machine <yellow.machine@disroot.org>
2022-06-21 13:53:57 +0000

    two sends TYPE, going to idle, buffering and idle again

--- test/machineTests/interpret.test.js
+++ test/machineTests/interpret.test.js
@@ -1,10 +1,9 @@
 const { interpret } = require("xstate");
 const remoteMachineFactory = require('../../src/lib/machine')
 
-const myfetch = jest.fn(async x => x)
 
 it('should reach initial from initial on TYPE', (done) => {
-    
+    const myfetch = jest.fn(async x => x)    
     const remoteMachine = remoteMachineFactory({myfetch})
 
     let count = 0;
@@ -21,6 +20,27 @@ it('should reach initial from initial on TYPE', (done) => {
 
     service.send('TYPE', {data: "xyz"});
     
-    //service.stop();
 });
   
+it('should reach initial from initial on two TYPE', (done) => {
+  const myfetch = jest.fn(async x => x)  
+  const remoteMachine = remoteMachineFactory({myfetch})
+
+  let count = 0;
+  const service = interpret(remoteMachine)
+    .onTransition(state => {
+      count++
+      if(state.matches("iddle") && state.context.buffer.length === 0 && state.context.current !== 'initial') {
+        expect(count).toBe(5)
+        expect(myfetch.mock.results[0].value).resolves.toBe('xyz');
+        expect(myfetch.mock.results[1].value).resolves.toBe('abc');
+        
+        done()
+      }
+    })
+    .start();
+
+  service.send('TYPE', {data: "xyz"});
+  service.send('TYPE', {data: "abc"});
+
+});
